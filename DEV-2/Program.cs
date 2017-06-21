583cb4c2002fa0c7d942b677da263444a0f667dd
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-06-21 16:13:07 +0300

    Commit DEV-2

---
+++ DEV-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                String numbers = ((i % 3 == 0) && (i % 5 != 0) ? "Tutti" : (i % 5 == 0) && (i % 3 != 0) ? "Frutti" : (i % 3 == 0) && (i % 5 == 0) ? "Tutti-Frutti" : i.ToString());
+                Console.Write(numbers + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}

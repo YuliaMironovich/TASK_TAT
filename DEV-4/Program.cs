778c0eec17732ef50ddbcbe1aaba6fc5e394c4c4
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-06-28 16:00:17 +0300

    task dev 4

---
+++ DEV-4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace DEV_4
+{
+    class EntryPoint
+    {
+        static void Main(string [] args)
+        {
+           bool duration = true;
+           while (duration)
+           {
+               try
+               {
+                   if (args.Length == 0)
+                   {
+                       Console.WriteLine("You did not enter values in the command line. Enter the sequence of integers: ");
+                       string enteredString = Console.ReadLine();
+                       string[] stringSequence = enteredString.Split(' ');
+                       SequenseOfNumbers sequenceOfNumbers = new SequenseOfNumbers();
+                       int[] sequence = sequenceOfNumbers.ConvertToInt(stringSequence);
+                       bool result = sequenceOfNumbers.IsSequenceNondecreasing(sequence);
+                       sequenceOfNumbers.OutputOfResult(result);
+                   }
+                   else
+                   {
+                       SequenseOfNumbers sequenceOfNumbers = new SequenseOfNumbers();
+                       int[] sequence = sequenceOfNumbers.ConvertToInt(args);
+                       bool result = sequenceOfNumbers.IsSequenceNondecreasing(sequence);
+                       sequenceOfNumbers.OutputOfResult(result);   
+                   }
+               }
+               catch (FormatException)
+               {
+                   Console.WriteLine("Enter integers!!Try again: \n");
+                   continue;
+               }
+               duration = false;
+           }
+        }
+
+    }
+}
---
+++ DEV-4/SequenceOFNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DEV_4
+{
+   public class SequenseOfNumbers
+    {
+       public int[] ConvertToInt(string[] stringSequence)
+       {
+          int[] sequence = new int[stringSequence.Length];
+          for (int i = 0; i < stringSequence.Length; i++)
+          {
+              sequence[i] = int.Parse(stringSequence[i]);
+          }
+          return sequence; 
+       }
+        public bool IsSequenceNondecreasing(int[] sequence)
+        {
+            for (int i = 0; i < sequence.Length - 1 ; i++)
+            {
+                if (sequence[i] > sequence[i + 1])
+                {
+                    return false;
+                }  
+            }
+            return true;
+        }
+   public void OutputOfResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("This sequence is non - decreasing.");
+            }
+            else
+            {
+                Console.WriteLine("This sequence is not non - decreasing.");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}

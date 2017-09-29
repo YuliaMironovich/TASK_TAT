efddda11f702f5884a1b72dca4fa5ab6006af43b
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-09-29 21:43:12 +0300

    Change the task DEV-9

--- DEV-9/CharArray.cs
+++ DEV-9/CharArray.cs
@@ -1,45 +1,34 @@
 ﻿using System;
-
+using System.Text;
 
 namespace DEV_9
 {
     class CharArray
     {
-        public char[] ReplaceSymbols(char[] first, char[] second)
+        public string ReplaceSymbols(char[] first, char[] second)
         {
-                Random rnd1 = new Random();
-                Random rnd2 = new Random(); 
-                int firstNumberForReplacing = rnd1.Next(1, first.Length);
-                int secondNumberForReplacing = rnd2.Next(1, second.Length);
-                int firstOrigin = rnd1.Next(0, first.Length - firstNumberForReplacing);
-                int secondOrigin = rnd2.Next(0, second.Length - secondNumberForReplacing);
-                int size = second.Length - secondNumberForReplacing + firstNumberForReplacing;
-                char[] result = new char[size];
-                for (int i = 0; i < secondOrigin; i++)
-                {
-                    result[i] = second[i];
-                }
-                int j = firstOrigin;
-                for (int i = secondOrigin; i < secondOrigin + firstNumberForReplacing; i++)
-                {
-                    result[i] = first[j];
-                    j++;
-                }
-                j = secondOrigin + secondNumberForReplacing;
-                for (int i = secondOrigin + firstNumberForReplacing; i < size; i++ )
-                {
-                    result[i] = second[j];
-                    j++;
-                }
-                return result;           
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+            int firstNumberForReplacing = rnd1.Next(1, first.Length);
+            int secondNumberForReplacing = rnd2.Next(1, second.Length);
+            int firstOrigin = rnd1.Next(0, first.Length - firstNumberForReplacing);
+            int secondOrigin = rnd2.Next(0, second.Length - secondNumberForReplacing);
+            string firstPartOfRow = new string(second, 0, secondOrigin);
+            string secondPartOfRow = new string(first, firstOrigin, firstNumberForReplacing);
+            int startIndex = secondOrigin + secondNumberForReplacing;
+            int number = second.Length - (secondOrigin + secondNumberForReplacing);
+            string thirdPartOfRow = new string(second, startIndex, number);
+            StringBuilder result = new StringBuilder();
+            result.Append(firstPartOfRow);
+            result.Append(secondPartOfRow);
+            result.Append(thirdPartOfRow);
+            return result.ToString();
         }
         
         public void Output(char[] array)
         {
-            foreach (char element in array)
-            {
-                Console.Write(element);
-            }
+            string row = new string(array);
+            Console.WriteLine(row);
             Console.WriteLine();
         }
     }
--- DEV-9/EntryPoint.cs
+++ DEV-9/EntryPoint.cs
@@ -1,36 +1,40 @@
 ﻿using System;
-
+using System.IO;
 
 namespace DEV_9
 {
     class EntryPoint
     {
         private const string ERROR = ("Check the data in file for reading!");
+        const string path = @"D:\универ\VS tat\DEV-9\FileWithStrings.txt";
         static void Main(string[] args)
         {
             try
             {
-                string path = @"D:\универ\VS tat\DEV-9\FileWithStrings.txt";
-                const int NUMBER_OF_ROWS = 2;
-                File file = new File();
-                string[] rows = file.Read(path, NUMBER_OF_ROWS);
-                char[] firstRow = rows[0].ToCharArray();
-                char[] secondRow = rows[1].ToCharArray();
+                string[] arrayOfLines = new string[2];
+                using (StreamReader streamReader = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    for (int i = 0; i < arrayOfLines.Length; i++)
+                    {
+                        arrayOfLines[i] = streamReader.ReadLine();
+                    };
+                }
+                char[] firstRow = arrayOfLines[0].ToCharArray();
+                char[] secondRow = arrayOfLines[1].ToCharArray();
                 CharArray array = new CharArray();
                 Console.WriteLine("The first row: ");
                 array.Output(firstRow);
                 Console.WriteLine("The second row: ");
                 array.Output(secondRow);
                 Console.WriteLine("Result row: ");
-                char[] result = array.ReplaceSymbols(firstRow, secondRow);
-                array.Output(result);
+                string result = array.ReplaceSymbols(firstRow, secondRow);
+                Console.WriteLine(result);
             }
             catch(Exception e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(ERROR);
             }
-
             Console.ReadKey();
         }
 

291a01f81c7816f9da473f94ebff5334022e9936
Yulia Mironovich <yulyaka.mironovich@gmail.com>
2017-09-29 12:36:23 +0300

    Add task DEV-9

---
+++ DEV-9/CharArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace DEV_9
+{
+    class CharArray
+    {
+        public char[] ReplaceSymbols(char[] first, char[] second)
+        {
+                Random rnd1 = new Random();
+                Random rnd2 = new Random(); 
+                int firstNumberForReplacing = rnd1.Next(1, first.Length);
+                int secondNumberForReplacing = rnd2.Next(1, second.Length);
+                int firstOrigin = rnd1.Next(0, first.Length - firstNumberForReplacing);
+                int secondOrigin = rnd2.Next(0, second.Length - secondNumberForReplacing);
+                int size = second.Length - secondNumberForReplacing + firstNumberForReplacing;
+                char[] result = new char[size];
+                for (int i = 0; i < secondOrigin; i++)
+                {
+                    result[i] = second[i];
+                }
+                int j = firstOrigin;
+                for (int i = secondOrigin; i < secondOrigin + firstNumberForReplacing; i++)
+                {
+                    result[i] = first[j];
+                    j++;
+                }
+                j = secondOrigin + secondNumberForReplacing;
+                for (int i = secondOrigin + firstNumberForReplacing; i < size; i++ )
+                {
+                    result[i] = second[j];
+                    j++;
+                }
+                return result;           
+        }
+        
+        public void Output(char[] array)
+        {
+            foreach (char element in array)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DEV-9/File.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace DEV_9
+{
+    public class File
+    {
+        public string[] Read(string path, int NUMBER_OF_ROWS)
+        {
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string[] arrayOfLines = new string[NUMBER_OF_ROWS];
+                    for (int i = 0; i < NUMBER_OF_ROWS; i++)
+                    {
+                        arrayOfLines[i] = streamReader.ReadLine();
+                    }
+                    return arrayOfLines;
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+
+    }
+}
